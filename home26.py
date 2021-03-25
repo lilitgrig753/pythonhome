feee4eb3745fc89da83689200ac70f30ea0b3883
Albertpython <80157870+Albertpython@users.noreply.github.com>
2021-03-25 14:14:32 +0400

    home26

---
+++ home26.py
@@ -0,0 +1,29 @@
+'''Write a Python program to add info in JSON file
+about you.'''
+
+# import json
+# file_name = "abul.json"
+# user = {'name':'Abul', 'age':21}
+# user_file = [user]
+# with open(file_name, 'w') as f:
+# 	json.dump(user_file, f)
+# f = open(file_name)
+# json_data = json.load(f)
+# for data in json_data:
+# 	print(data)
+
+
+'''Write a Python program to check if your json file
+have this info.'''
+
+# import json
+
+# name = input('Name: ')
+# with open('abul.json', 'r') as file:
+#     data = json.load(file)
+#     print(data)
+# for i in range(len(data)):
+# 	if data[i]['name'] == name:
+# 		print('yes') 
+# 	else:
+# 		print('No')
